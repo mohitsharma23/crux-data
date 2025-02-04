2583f0724223ca4649f47730b335373441f90034
Mohit Sharma <mohitsharma23j97@gmail.com>
2025-02-04 21:54:26 +0530

    feat: update transform data

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,26 +11,27 @@ export function transformCruxData(cruxResponse: any) {
   return {
     origin: key.origin,
     fcp: {
-      value: metrics.first_contentful_paint?.percentiles?.p75 || "N/A",
+      value: +metrics.first_contentful_paint?.percentiles?.p75 || "N/A",
       histogram: metrics.first_contentful_paint?.histogram,
     },
     lcp: {
-      value: metrics.largest_contentful_paint?.percentiles?.p75 || "N/A",
+      value: +metrics.largest_contentful_paint?.percentiles?.p75 || "N/A",
       histogram: metrics.largest_contentful_paint?.histogram,
     },
     inp: {
-      value: metrics.interaction_to_next_paint?.percentiles?.p75 || "N/A",
+      value: +metrics.interaction_to_next_paint?.percentiles?.p75 || "N/A",
       histogram: metrics.interaction_to_next_paint?.histogram,
     },
     cls: {
-      value: metrics.cumulative_layout_shift?.percentiles?.p75 || "N/A",
+      value: +metrics.cumulative_layout_shift?.percentiles?.p75 || "N/A",
       histogram: metrics.cumulative_layout_shift?.histogram,
     },
     ttfb: {
-      value: metrics.experimental_time_to_first_byte?.percentiles?.p75 || "N/A",
+      value:
+        +metrics.experimental_time_to_first_byte?.percentiles?.p75 || "N/A",
       histogram: metrics.experimental_time_to_first_byte?.histogram,
     },
-    rtt: metrics.round_trip_time?.percentiles?.p75 || "N/A",
+    rtt: +metrics.round_trip_time?.percentiles?.p75 || "N/A",
     collectionPeriod: `${collectionPeriod.firstDate.year}-${collectionPeriod.firstDate.month}-${collectionPeriod.firstDate.day} → ${collectionPeriod.lastDate.year}-${collectionPeriod.lastDate.month}-${collectionPeriod.lastDate.day}`,
   };
 }
